3d31ac32ae985e9843e67efab665c4d88576065b
Muhamad yazid zinky arisona <144634381+yazidzky@users.noreply.github.com>
2024-11-17 17:21:08 +0700

    Update script.js

--- script.js
+++ script.js
@@ -217,6 +217,20 @@ window.addEventListener('resize', () => {
 });
 
 
+// Menambahkan efek transparansi pada scroll
+window.addEventListener('scroll', function() {
+  const navbar = document.getElementById('navbar');
+  
+  if (window.scrollY > 50) {
+      // Saat scroll lebih dari 50px, navbar menjadi lebih transparan
+      navbar.style.background = "rgba(79, 0, 0, 0.8)";
+      navbar.style.opacity = "0.8";  // Mengurangi opacity untuk efek transparansi lebih jelas
+  } else {
+      // Saat di atas 50px, navbar transparan
+      navbar.style.background = "rgba(79, 0, 0, 0.5)";
+      navbar.style.opacity = "1";  // Mengembalikan opacity ke normal
+  }
+});
 
 
 
